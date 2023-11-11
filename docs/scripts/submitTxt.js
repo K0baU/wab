4ef4b38d1baac70eda293983f1c475c5974d33ba
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-11 18:19:59 +0900

    Add files via upload

---
+++ docs/scripts/submitTxt.js
@@ -0,0 +1,18 @@
+import { doc } from "esm/doc.js";
+import { addContent } from "esm/add-content.js";
+
+const submitContent = async () => {
+            const str = doc.messageInputBox.value;
+            const content = new Blob([str], { type: "text/plain" });
+            addContent("content", content);
+            doc.messageInputBox.value = "";
+        };
+        doc.sendForm.onsubmit = e => {
+            e.preventDefault();
+            submitContent();
+        };
+        doc.messageInputBox.onkeydown = (e) => {
+            if (e.key == "Enter" && e.metaKey) {
+                submitContent();
+            }
+        }
