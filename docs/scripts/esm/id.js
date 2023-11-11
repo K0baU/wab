7881900c74e7ac447da8073f6dd6d8b592e0f7a9
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-12 08:16:39 +0900

    Add files via upload

--- docs/scripts/esm/id.js
+++ docs/scripts/esm/id.js
@@ -1,7 +1,7 @@
 export const cid = async rec =>
     (new Uint8Array(await crypto.subtle.digest("SHA-256",
         rec.type ? await rec.arrayBuffer() : rec))).toString();
-const chars = "234679abcdefghijkmnpqrstuvwxyzACDEFGHJKLMNPQRTUVWXYZあいうえおかきくけこさしすせそたちつてとなにぬねのはひふほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶぼぱぴぷぽアイウオキクケコサシスセソチツテトナヌネノハヒフホマムメモヤユヨラリルレワヲンガギグゲゴザジズゼゾダヂヅデドバビブボパピプポ右雨円王音下火花貝学気九休玉金空月犬見五校左山子四糸字耳七車手十出女小上森人水正生青石赤千川先早草足村大男竹中虫町天田土日入年白八百文木本名目立林六";e
+const chars = "234679abcdefghijkmnpqrstuvwxyzACDEFGHJKLMNPQRTUVWXYZあいうえおかきくけこさしすせそたちつてとなにぬねのはひふほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶぼぱぴぷぽアイウオキクケコサシスセソチツテトナヌネノハヒフホマムメモヤユヨラリルレワヲンガギグゲゴザジズゼゾダヂヅデドバビブボパピプポ右雨円王音下火花貝学気九休玉金空月犬見五校左山子四糸字耳七車手十出女小上森人水正生青石赤千川先早草足村大男竹中虫町天田土日入年白八百文木本名目立林六";
 export const encodeId = (str) => {
     const arr = str.split(",");
     let rtn = "";
--- docs/scripts/esm/log.js
+++ docs/scripts/esm/log.js
@@ -1,3 +1,5 @@
+import { doc } from "./doc.js";
+
 export const log = (text) => {
     const p = document.createElement("p");
     p.append(text);
