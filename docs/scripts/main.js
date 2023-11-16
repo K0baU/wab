b45511f2dfa29bdd928bdb194850bee16bec8c58
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-16 11:54:36 +0900

    Add files via upload

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,18 +1,9 @@
-import { log } from "./esm/log.js";
 import { doc } from "./esm/doc.js";
+import { display } from "./esm/display.js";
 import { opr } from "./esm/db.js";
-import { displayContent } from "./esm/content-to-html.js";
-import { displayPeer } from "./esm/peer-to-html.js";
-import { getThread, getTag } from "./esm/search.js";
 
 window.onerror = log;
-    const display = () => {
-        log("display");
-        doc.contents.textContent = "";
-        if (!getThread(doc.messageInputBox.value)) if (!getTag()) {
-            opr.for({ store: "contents", index:"date", direction: "prev", f: displayContent});
-        }
-    };
+
     display();
     opr.for({ store: "credits", f: value => {
         if (value.name == "" && value.credit == 0) {
