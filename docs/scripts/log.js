7e9ce49f0e24be9a69722ef196e9a4c1a7ad5b30
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-17 17:17:34 +0900

    Add files via upload

---
+++ docs/scripts/log.js
@@ -0,0 +1,9 @@
+import { doc } from "./doc.js";
+
+export const log = (text) => {
+    const p = document.createElement("p");
+    p.append(text);
+    doc.logElm.append(p);
+};
+
+window.onerror = log;
