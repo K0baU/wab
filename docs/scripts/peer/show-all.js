369892d06e00114c217bc8c82d718b11dfc1e79a
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-18 11:29:40 +0900

    Delete docs/scripts/peer/show-all.js

--- docs/scripts/peer/show-all.js
+++
@@ -1,8 +0,0 @@
-import { showOne } from "./show-one.js";
-import { opr } from "../lib/db.js";
-
-opr.for({ store: "peers", f: value => {
-    if (value.name == "" && value.credit == 0) {
-        opr.crud({ store: "peers", op: "delete" });
-    }
-}, end: () => opr.for({ store: "peers", f: showOne }) });
