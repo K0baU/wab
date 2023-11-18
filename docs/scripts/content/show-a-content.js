12ebbd92156576d0001a285ab9121cb28e652eca
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-18 11:18:12 +0900

    Add files via upload

---
+++ docs/scripts/content/show-a-content.js
@@ -0,0 +1,57 @@
+import { log } from "../lib/log.js";
+import { doc, addDOM } from "../lib/doc.js";
+import { encodeId } from "./id.js";
+import { aPtn, tagPtn } from "./patterns.js";
+
+const HTMLify = (str, ptns) => {
+        return ptns.reduce((prev, ptn) =>
+            prev.replaceAll(RegExp(ptn, "g"), match =>
+                `<button type="button" onclick="
+                    arguments[0].stopImmediatePropagation();
+                    const box=document.getElementById('message');
+                    box.value += '${match.replaceAll("\n", " ")}';
+                    box.dispatchEvent(new InputEvent('input'));
+                    box.focus();">${match}</button>`
+            )
+            , str);
+    };
+
+export const showAContent = async result => {
+        const li = document.createElement("li");
+        doc("contentsUl").append(li);    
+        addDOM(li, [{ tag: "span", content: (new Date(result.date)).toLocaleString("ja") }]);
+        const file = result.body;
+        switch (file.type.split("/")[0]) {
+            case "text":
+                const p = document.createElement("p");
+                p.innerHTML = HTMLify(await file.text(), [aPtn, tagPtn]);
+                addDOM(li, [p]);
+                break;
+            case "image":
+                const img = document.createElement("img");
+                img.src = URL.createObjectURL(file);
+                img.onload = () => {
+                    URL.revokeObjectURL(img.src);
+                };
+                addDOM(li, [img]);
+                break;
+            case "video":
+                const video = document.createElement("video");
+                video.src = URL.createObjectURL(file);
+                video.controls = true;
+                video.onload = () => {
+                    URL.revokeObjectURL(video.src);
+                };
+                addDOM(li, [video]);
+                break;
+            default:
+                break;
+        }
+        li.onclick = () => {
+            if (getSelection().toString()) return;
+            const messageInput = doc("messageInput");
+            messageInput.value += `>>${encodeId(result.id)} `;
+            messageInput.dispatchEvent(new InputEvent('input'));
+            messageInput.focus();
+        };
+    };
---
+++ docs/scripts/content/show-contents.js
@@ -0,0 +1,33 @@
+import { doc } from "../lib/doc.js";
+import { opr } from "../lib/db.js";
+import { showAContent } from "./show-one.js";
+import { decodeId } from "./id.js";
+import { aPtn, tagPtn } from "./patterns.js";
+
+const messageInput = doc("messageInput");
+export const getThread = (content = messageInput.value) => {
+    const anchor = content.match(aPtn);
+    if (!anchor) return false;
+    const id = decodeId(anchor[1]);
+    opr.crud({ store: "contents", op: "get", rec: id, callback: async rec => {
+        showAContent(rec);
+        getThread(await rec.body.text());
+    } });
+    return true;
+};
+export const getTag = () => {
+    const tag = messageInput.value.match(tagPtn);
+    if (!tag) return false;
+    opr.for({ store: "contents", index:"tag", range: IDBKeyRange.only(tag[1]), f: showAContent});
+    return true;
+};
+
+export const showContents = () => {
+    doc("contentsUl").textContent = "";
+    if (!getThread()) if (!getTag()) {
+        opr.for({ store: "contents", index:"date", direction: "prev", f: showAContent});
+    }
+};
+
+showContents();
+messageInputBox = showContents;
